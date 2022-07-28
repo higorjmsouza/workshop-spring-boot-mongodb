b6468759e0318f356f2a310930ce1b3627a282be
Higor Jacinto <higorjmsouza@gmail.com>
2022-07-28 02:24:24 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/courseudemymongodb/services/UserService.java
+++ src/main/java/com/courseudemymongodb/services/UserService.java
@@ -30,6 +30,10 @@ public class UserService {
 		return repo.insert(obj);
 	}
 
+	public void delete(String id) {
+		repo.delete(findById(id));
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}

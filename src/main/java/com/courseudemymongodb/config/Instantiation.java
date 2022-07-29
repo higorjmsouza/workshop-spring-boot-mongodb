4784ad3005860210207d97f975d8cc74be02dafa
Higor Jacinto <higorjmsouza@gmail.com>
2022-07-28 21:28:40 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/courseudemymongodb/config/Instantiation.java
+++ src/main/java/com/courseudemymongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.courseudemymongodb.domain.Post;
 import com.courseudemymongodb.domain.User;
 import com.courseudemymongodb.dto.AuthorDTO;
+import com.courseudemymongodb.dto.CommentDTO;
 import com.courseudemymongodb.repository.PostRepository;
 import com.courseudemymongodb.repository.UserRepository;
 
@@ -42,10 +43,18 @@ public class Instantiation implements CommandLineRunner {
 				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Boa viagem mano!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+
+		post2.getComments().add(c3);
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		
+
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
-		
+
 		userRepository.save(maria);
 
 	}
